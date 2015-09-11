5dbb31f2933b0dd401a3167b1875ecff33ddeef1
clux <analsandblaster@gmail.com>
2015-09-11 19:11:54 +0100

    rewrite using a proper algorithm - fixes #1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Quadratic
 [![build status](https://secure.travis-ci.org/clux/quadratic.svg)](http://travis-ci.org/clux/quadratic)
 
-Number theoretic experiments with rust.
+Number theoretic explorations with rust. Calculates the [Legendre](http://mathworld.wolfram.com/LegendreSymbol.html) and [Jacobi symbol](http://mathworld.wolfram.com/JacobiSymbol.html) using the [standard algorithm](https://en.wikipedia.org/wiki/Jacobi_symbol#Calculating_the_Jacobi_symbol). Since the Jacobi symbol is a generalization of the Legendre symbol, only this function is provided.
 
 ## Usage
 Add as a git dependency (for now), and import quadratic:
@@ -9,6 +9,6 @@ Add as a git dependency (for now), and import quadratic:
 ```rust
 extern crate quadratic;
 
-let ls = quadratic::legendre(2, 5);
-let js = quadratic::jacob(2, 15);
+assert_eq!(-1, quadratic::jacobi(2, 5));
+assert_eq(1, quadratic::jacobi(2, 15));
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,114 @@
-extern crate primes;
+extern crate num;
+use num::Integer;
 
-/// Returns the value of the Legendre symbol for (a \ p)
-/// Where a is an integer and p is an odd prime number
-///
-///
-/// # Examples
-///
-/// ```rust
-/// # extern crate quadratic;
-/// // compute the Legendre symbol of (2 \ 5)
-/// let symb = quadratic::legendre(2, 5);
-/// # assert_eq!(-1, symb);
-/// ```
+fn two_over(n: u32) -> (i8) {
+    if n % 8 == 1 || n % 8 == 7 {
+        return 1;
+    }
+    return -1; // otherwise n is 3 or 5 mod 8 since n is odd
+}
 
-pub fn legendre(a: i32, p: u32) -> (i8) {
-    assert!(p % 2 == 1); // also assume p is prime - but silly to check here..
-    let mut a_ = a;
-    while a_ < 0 {
-        a_ += p as i32;
+fn reciprocity(num: u32, den: u32) -> (i8) {
+    if num % 4 == 3 && den % 4 == 3 {
+        return -1;
     }
-    let b = a_ as u32; // safe to assume >= 0 now
-    let ls = b.pow((p-1)/2) % p; // can use u32 version of pow + rem..
-    if ls == p - 1 {
-      return -1 as i8;
-    }
-    ls as i8
+    return 1;
 }
 
 /// Returns the value of the Jacobi symbol for (a \ n)
-/// Where a is an integer and n is an odd integer > 2
+/// Where a is an integer and n is an odd positive integer
 ///
 ///
 /// # Examples
 ///
 /// ```rust
 /// # extern crate quadratic;
-/// // compute the Jacobi symbol of (2 \ 15)
-/// let symb = quadratic::jacobi(2, 15);
-/// # assert_eq!(1, symb);
+/// // compute the Jacobi symbol of (2 \ 5)
+/// let symb = quadratic::jacobi(2, 5);
+/// # assert_eq!(-1, symb);
 /// ```
 
-pub fn jacobi(a: i32, n: u32) -> (i8) {
-    assert!(n % 2 == 1);
-    primes::factors(n as u64).iter().fold(1, |acc, &el| acc * legendre(a, el as u32))
-}
+pub fn jacobi(a_: i32, n_: i32) -> (i8) {
+    assert!(n_.is_odd());
+    assert!(n_ > 0);
+    // ensure we start out with u32s
+    let a = a_.mod_floor(&n_) as u32;
+    let n = n_ as u32;
 
+    // start algorithm:
+    let mut acc = 1;
+    let mut num = a;
+    let mut den = n;
+    loop {
+        // reduce numerator
+        num = num % den;
+        if num == 0 {
+            return 0;
+        }
+
+        // extract factors of two from numerator
+        while num.is_even() {
+            acc *= two_over(den);
+            num /= 2;
+        }
+        // if numerator is 1 => this sub-symbol is 1
+        if num == 1 {
+            return acc;
+        }
+        // shared factors => one sub-symbol is zero
+        if num.gcd(&den) > 1 {
+            return 0;
+        }
+        // num and den are now odd co-prime, use reciprocity law:
+        acc *= reciprocity(num, den);
+        let tmp = num;
+        num = den;
+        den = tmp;
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use legendre;
+    use jacobi;
 
+    // legendre tests
     #[test]
     fn minus_one_over_p() {
         // 1 mod 4 => 1
-        assert_eq!(1, legendre(-1, 5));
-        assert_eq!(1, legendre(-1, 13));
+        assert_eq!(1, jacobi(-1, 5));
+        assert_eq!(1, jacobi(-1, 13));
         // 3 mod 4 => -1
-        assert_eq!(-1, legendre(-1, 3));
-        assert_eq!(-1, legendre(-1, 7));
+        assert_eq!(-1, jacobi(-1, 3));
+        assert_eq!(-1, jacobi(-1, 7));
     }
     #[test]
     fn two_over_p() {
-        assert_eq!(-1, legendre(2, 3));
-        assert_eq!(-1, legendre(2, 5));
-        assert_eq!(1, legendre(2, 7));
-        assert_eq!(1, legendre(2, 17)); // 17 = 1 mod 8
+        assert_eq!(-1, jacobi(2, 3));
+        assert_eq!(-1, jacobi(2, 5));
+        assert_eq!(1, jacobi(2, 7));
+        assert_eq!(1, jacobi(2, 17)); // 17 = 1 mod 8
     }
     #[test]
     fn three_over_p() {
-        assert_eq!(0, legendre(3, 3));
-        assert_eq!(-1, legendre(3, 5));
-        assert_eq!(-1, legendre(3, 7));
+        assert_eq!(0, jacobi(3, 3));
+        assert_eq!(-1, jacobi(3, 5));
+        assert_eq!(-1, jacobi(3, 7));
     }
     #[test]
-    fn legendre_periodicity() {
-        assert_eq!(legendre(3,5), legendre(-2,5));
-        assert_eq!(legendre(-1,5), legendre(4,5));
-        assert_eq!(legendre(11,7), legendre(4,7));
-        assert_eq!(legendre(-3,7), legendre(4,7));
-        assert_eq!(legendre(10,7), legendre(3,7));
+    fn periodicity() {
+        assert_eq!(jacobi(3,5), jacobi(-2,5));
+        assert_eq!(jacobi(-1,5), jacobi(4,5));
+        assert_eq!(jacobi(11,7), jacobi(4,7));
+        assert_eq!(jacobi(-3,7), jacobi(4,7));
+        assert_eq!(jacobi(10,7), jacobi(3,7));
     }
 
-    use jacobi;
+    // jacobi tests
     #[test]
-    fn a_over_45() { // 45 = 3*3*5
+    fn a_over_n() { // 45 = 3*3*5
         assert_eq!(-1, jacobi(2, 45)); // -1 * -1 * -1
         assert_eq!(0, jacobi(3, 45)); // 0 * 0 * -1
         assert_eq!(-1, jacobi(7, 45)); // -1 * -1 * -1
+        assert_eq!(1, jacobi(2, 15)); // (2\5) * (2\3)
     }
 }
